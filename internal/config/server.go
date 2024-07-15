3ca8f59da9875e581042ebcb0b103226dc70b24e
Stas Merzlyakov <merzliakov@gmail.com>
2024-07-15 20:20:53 +0400

    доп.параметры

--- internal/config/server.go
+++ internal/config/server.go
@@ -1,24 +1,43 @@
 package config
 
-import "github.com/caarlos0/env"
+import (
+	"time"
+
+	"github.com/caarlos0/env"
+)
 
 const (
-	ServerDefaultPort    = ":3200"
-	ServerDefaultTLSKey  = ""
-	ServerDefaultTLSCert = ""
+	ServerDefaultPort        = ":3200"
+	ServerDefaultTLSKey      = ""
+	ServerDefaultTLSCert     = ""
+	ServerDefaultTokenExp    = 3 * time.Hour
+	ServerDefaultTokenSecret = "secret"
+	ServerDefaultAuthTimeout = 10 * time.Minute
+	ServerDefaultMasterKey   = "MasterKey!"
+	ServerDefaultDomainName  = "localhost"
 )
 
 type ServerConf struct {
-	Port    string `env:"PORT"`
-	TLSKey  string `env:"TLS_KEY"`
-	TLSCert string `env:"TLS_CERT"`
+	Port        string        `env:"PORT"`
+	TLSKey      string        `env:"TLS_KEY"`
+	TLSCert     string        `env:"TLS_CERT"`
+	TokenExp    time.Duration `env:"JWT_EXP"`
+	TokenSecret string        `env:"JWT_SECRET"`
+	AuthTimeout time.Duration `env:"AUTH_TIMEOUT"`
+	MasterKey   string        `env:"MASTER_KEY"`
+	DomainName  string        `env:"DOMAIN_NAME"`
 }
 
 func defaultServConf() *ServerConf {
 	return &ServerConf{
-		Port:    ServerDefaultPort,
-		TLSKey:  ServerDefaultTLSKey,
-		TLSCert: ServerDefaultTLSCert,
+		Port:        ServerDefaultPort,
+		TLSKey:      ServerDefaultTLSKey,
+		TLSCert:     ServerDefaultTLSCert,
+		TokenExp:    ServerDefaultTokenExp,
+		TokenSecret: ServerDefaultTokenSecret,
+		AuthTimeout: ServerDefaultAuthTimeout,
+		MasterKey:   ServerDefaultMasterKey,
+		DomainName:  ServerDefaultDomainName,
 	}
 }
 
--- internal/config/server_test.go
+++ internal/config/server_test.go
@@ -3,6 +3,7 @@ package config_test
 import (
 	"os"
 	"testing"
+	"time"
 
 	"github.com/StasMerzlyakov/gophkeeper/internal/config"
 	"github.com/stretchr/testify/require"
@@ -18,6 +19,13 @@ func TestLoadServConf(t *testing.T) {
 		assert.Equal(t, config.ServerDefaultPort, conf.Port)
 		assert.Equal(t, config.ServerDefaultTLSCert, conf.TLSCert)
 		assert.Equal(t, config.ServerDefaultTLSKey, conf.TLSKey)
+
+		assert.Equal(t, config.ServerDefaultTokenExp, conf.TokenExp)
+		assert.Equal(t, config.ServerDefaultTokenSecret, conf.TokenSecret)
+		assert.Equal(t, config.ServerDefaultAuthTimeout, conf.AuthTimeout)
+
+		assert.Equal(t, config.ServerDefaultMasterKey, conf.MasterKey)
+		assert.Equal(t, config.ServerDefaultDomainName, conf.DomainName)
 	})
 
 	t.Run("env values", func(t *testing.T) {
@@ -25,11 +33,24 @@ func TestLoadServConf(t *testing.T) {
 		os.Setenv("TLS_KEY", "test.key")
 		os.Setenv("TLS_CERT", "test.cert")
 
+		os.Setenv("JWT_EXP", "1h")
+		os.Setenv("JWT_SECRET", "pass")
+		os.Setenv("AUTH_TIMEOUT", "5m")
+
+		os.Setenv("MASTER_KEY", "key")
+		os.Setenv("DOMAIN_NAME", "example.com")
+
 		conf, err := config.LoadServConf()
 		require.NoError(t, err)
 
 		assert.Equal(t, ":9191", conf.Port)
 		assert.Equal(t, "test.cert", conf.TLSCert)
 		assert.Equal(t, "test.key", conf.TLSKey)
+
+		assert.Equal(t, 1*time.Hour, conf.TokenExp)
+		assert.Equal(t, "pass", conf.TokenSecret)
+		assert.Equal(t, 5*time.Minute, conf.AuthTimeout)
+		assert.Equal(t, "key", conf.MasterKey)
+		assert.Equal(t, "example.com", conf.DomainName)
 	})
 }
