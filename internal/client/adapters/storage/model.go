396c8e6842dddaaa6fc74a1e6b3e61030b9be819
Stas Merzlyakov <merzliakov@gmail.com>
2024-07-29 20:11:13 +0400

    реализовал хранилище данных на стороне клинета

--- internal/client/adapters/storage/model.go
+++ internal/client/adapters/storage/model.go
@@ -1,18 +1,24 @@
 package storage
 
 import (
+	"fmt"
+
 	"github.com/StasMerzlyakov/gophkeeper/internal/domain"
 )
 
 func NewStorage() *simpleStorage {
 	return &simpleStorage{
-		status: domain.ClientStatusOnline,
+		status:           domain.ClientStatusOnline,
+		userPasswordData: make(map[string]domain.UserPasswordData),
+		bankCards:        make(map[string]domain.BankCard),
 	}
 }
 
 type simpleStorage struct {
-	masterPassword string
-	status         domain.ClientStatus
+	masterPassword   string
+	status           domain.ClientStatus
+	userPasswordData map[string]domain.UserPasswordData
+	bankCards        map[string]domain.BankCard
 }
 
 func (ss *simpleStorage) SetMasterPassword(masterPassword string) {
@@ -21,4 +27,96 @@ func (ss *simpleStorage) SetMasterPassword(masterPassword string) {
 
 func (ss *simpleStorage) GetMasterPassword() string {
 	return ss.masterPassword
+}
+
+func (ss *simpleStorage) AddBankCard(bankCard *domain.BankCard) error {
+	if _, ok := ss.bankCards[bankCard.Number]; ok {
+		// Method on client invoked after success server method invokaction, so it's client error.
+		return fmt.Errorf("%w bankCard with number %v exists, reopen client", domain.ErrClientInternal, bankCard.Number)
+	}
+	ss.bankCards[bankCard.Number] = *bankCard
+	return nil
+}
+
+func (ss *simpleStorage) AddUserPasswordData(data *domain.UserPasswordData) error {
+	if _, ok := ss.userPasswordData[data.Hint]; ok {
+		// Method on client invoked after success server method invokaction, so it's client error.
+		return fmt.Errorf("%w userPasswordData with hint %v exists, reopen client", domain.ErrClientInternal, data.Hint)
+	}
+	ss.userPasswordData[data.Hint] = *data
+	return nil
+}
+
+func (ss *simpleStorage) UpdateBankCard(bankCard *domain.BankCard) error {
+	if _, ok := ss.bankCards[bankCard.Number]; !ok {
+		// Method on client invoked after success server method invokaction, so it's client error.
+		return fmt.Errorf("%w bankCard with number %v is not exists, reopen client", domain.ErrClientInternal, bankCard.Number)
+	}
+	ss.bankCards[bankCard.Number] = *bankCard
+	return nil
+}
+
+func (ss *simpleStorage) UpdatePasswordData(data *domain.UserPasswordData) error {
+	if _, ok := ss.userPasswordData[data.Hint]; !ok {
+		// Method on client invoked after success server method invokaction, so it's client error.
+		return fmt.Errorf("%w userPasswordData with hint %v is not exists, reopen client", domain.ErrClientInternal, data.Hint)
+	}
+	ss.userPasswordData[data.Hint] = *data
+	return nil
+}
+
+func (ss *simpleStorage) DeleteBankCard(number string) error {
+	if _, ok := ss.bankCards[number]; !ok {
+		// Method on client invoked after success server method invokaction, so it's client error.
+		return fmt.Errorf("%w bankCard with number %v is not exists, reopen client", domain.ErrClientInternal, number)
+	}
+	delete(ss.bankCards, number)
+	return nil
+}
+
+func (ss *simpleStorage) DeleteUpdatePasswordData(hint string) error {
+	if _, ok := ss.userPasswordData[hint]; !ok {
+		// Method on client invoked after success server method invokaction, so it's client error.
+		return fmt.Errorf("%w userPasswordData with hint %v is not exists, reopen client", domain.ErrClientInternal, hint)
+	}
+	delete(ss.userPasswordData, hint)
+	return nil
+}
+
+func (ss *simpleStorage) GetBankCard(number string) (*domain.BankCard, error) {
+	if card, ok := ss.bankCards[number]; !ok {
+		// Method on client invoked after success server method invokaction, so it's client error.
+		return nil, fmt.Errorf("%w bankCard with number %v is not exists, reopen client", domain.ErrClientInternal, number)
+	} else {
+		return &card, nil
+	}
+}
+
+func (ss *simpleStorage) GetUpdatePasswordData(hint string) (*domain.UserPasswordData, error) {
+	if data, ok := ss.userPasswordData[hint]; !ok {
+		// Method on client invoked after success server method invokaction, so it's client error.
+		return nil, fmt.Errorf("%w userPasswordData with hint %v is not exists, reopen client", domain.ErrClientInternal, hint)
+	} else {
+		return &data, nil
+	}
+}
+
+func (ss *simpleStorage) GetBankCardNumberList() []string {
+	keys := make([]string, len(ss.bankCards))
+	i := 0
+	for k := range ss.bankCards {
+		keys[i] = k
+		i++
+	}
+	return keys
+}
+
+func (ss *simpleStorage) GetUserPasswordDataList() []string {
+	keys := make([]string, len(ss.userPasswordData))
+	i := 0
+	for k := range ss.userPasswordData {
+		keys[i] = k
+		i++
+	}
+	return keys
 }
--- internal/client/adapters/storage/model_test.go
+++ internal/client/adapters/storage/model_test.go
@@ -4,7 +4,9 @@ import (
 	"testing"
 
 	"github.com/StasMerzlyakov/gophkeeper/internal/client/adapters/storage"
+	"github.com/StasMerzlyakov/gophkeeper/internal/domain"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestApp(t *testing.T) {
@@ -14,5 +16,77 @@ func TestApp(t *testing.T) {
 		mKey := "MasterPassword"
 		app.SetMasterPassword(mKey)
 		assert.Equal(t, mKey, app.GetMasterPassword())
+	})
+
+	t.Run("bank_card_operations", func(t *testing.T) {
+		app := storage.NewStorage()
+
+		require.Equal(t, 0, len(app.GetBankCardNumberList()))
+
+		card := &domain.BankCard{
+			Number:      "6250941006528599",
+			ExpiryMonth: 06,
+			ExpiryYear:  2026,
+			CVV:         "123",
+		}
+
+		err := app.AddBankCard(card)
+		require.NoError(t, err)
+
+		require.Equal(t, 1, len(app.GetBankCardNumberList()))
+
+		err = app.AddBankCard(card)
+		require.ErrorIs(t, err, domain.ErrClientInternal)
+
+		crd, err := app.GetBankCard(card.Number)
+		require.NoError(t, err)
+
+		require.Equal(t, crd.CVV, card.CVV)
+		require.Equal(t, crd.Number, card.Number)
+		require.Equal(t, crd.ExpiryMonth, card.ExpiryMonth)
+		require.Equal(t, crd.ExpiryYear, card.ExpiryYear)
+
+		err = app.DeleteBankCard("6250941006528598")
+		require.ErrorIs(t, err, domain.ErrClientInternal)
+
+		err = app.DeleteBankCard("6250941006528599")
+		require.NoError(t, err)
+
+		require.Equal(t, 0, len(app.GetBankCardNumberList()))
+	})
+
+	t.Run("user_data_operations", func(t *testing.T) {
+		app := storage.NewStorage()
+
+		require.Equal(t, 0, len(app.GetUserPasswordDataList()))
+
+		data := &domain.UserPasswordData{
+			Hint:     "ya.ru",
+			Login:    "login",
+			Passwrod: "pass",
+		}
+
+		err := app.AddUserPasswordData(data)
+		require.NoError(t, err)
+
+		require.Equal(t, 1, len(app.GetUserPasswordDataList()))
+
+		err = app.AddUserPasswordData(data)
+		require.ErrorIs(t, err, domain.ErrClientInternal)
+
+		dt, err := app.GetUpdatePasswordData(data.Hint)
+		require.NoError(t, err)
+
+		require.Equal(t, dt.Hint, data.Hint)
+		require.Equal(t, dt.Login, data.Login)
+		require.Equal(t, dt.Passwrod, data.Passwrod)
+
+		err = app.DeleteUpdatePasswordData("yayyy")
+		require.ErrorIs(t, err, domain.ErrClientInternal)
+
+		err = app.DeleteUpdatePasswordData("ya.ru")
+		require.NoError(t, err)
+
+		require.Equal(t, 0, len(app.GetUserPasswordDataList()))
 	})
 }
